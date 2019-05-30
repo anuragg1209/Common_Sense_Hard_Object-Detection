25377aef965a9ef50e2d27e3219e50d1766e60ab
Anurag garg <50840934+anuragg1209@users.noreply.github.com>
2019-05-31 00:00:36 +0530

    Update json_to_csv.py

--- json_to_csv.py
+++ json_to_csv.py
@@ -1,46 +1,67 @@
 import os
 import json
 import csv
+import argparse
 
+def convert_json(in_dir,out_dir,yolo_dir):
 
-def fun(in_dir,out_dir,**d):
-
-    os.chdir("C:/Users/anurag/Downloads/darkflow-master/darkflow-master") #change directory to that path where object detector YOLO v2 resides.
+    os.chdir(yolo_dir) #change directory to that path where object detector YOLO v2 resides.
     if not os.path.exists(in_dir):
         print("INVALID DIRECTORY ENTERED")
     else:
         os.system("python flow --imgdir "+in_dir+"/ --model cfg/yolo.cfg --load bin/yolo.weights --json")
 
-        filelst = os.listdir(in_dir+"/out")
+        filelst = os.listdir(in_dir+"/out")#storing all the names of the json files in a list 
 
         if not os.path.exists(out_dir):
             os.mkdir(out_dir)
 
         for j in filelst:
-            infile = open(os.path.join(in_dir+"/out", j), "r")  # input the json file directory
-            data = json.loads(infile.read())
+            with open(os.path.join(in_dir + "/out", j), "r") as infile:  # input the json file directory
+                for line in infile:
+                    data = json.loads(line)
+                        # creating corresponding .csv file
+                    assert j.split(".")[0] !=None, "value at index 0 not present"
+                    image_data = open(out_dir + "/" + j.split(".")[0] + ".csv",'w')#if file name is "demo.json" j.split(".")[0] will return "demo" as the output
 
-            image_data = open(out_dir +"/"+ j.split(".")[0] + ".csv", 'w')  # creating corresponding .csv file
+                    # create the csv writer object
 
-            # create the csv writer object
+                    csvwriter = csv.writer(image_data)
 
-            csvwriter = csv.writer(image_data)
+                     header = ["label", "confidence", "top_left_x", "top_left_y", "bottom_right_x","bottom_right_y"]  # list containing column names
+                     csvwriter.writerow(header)
+                     for i in data:
+                        s = list(i.values())
+                        l = [s[0], s[1], s[2]["x"], s[2]["y"], s[3]["x"], s[3]["y"]]
+                        csvwriter.writerow(l)
 
-            header = ["label", "confidence", "top_left_x", "top_left_y", "bottom_right_x","bottom_right_y"]  # list containing column names
-            csvwriter.writerow(header)
-            for i in data:
-                s = list(i.values())
-                l = [s[0], s[1], s[2]["x"], s[2]["y"], s[3]["x"], s[3]["y"]]
-                csvwriter.writerow(l)
-
-            image_data.close()
-
-
-if __name__=="__main__":
-    in_dir_path=input("enter input directory path where images are stored")
-    out_dir_path=input("enter output directory path where csv file will be stored")
-    fun(in_dir_path,out_dir_path)
+                     image_data.close()
 
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Code to convert json to csv',
+                                     usage="\n\npython json_to_csv.py"
+                                           "\t --input_dir "
+                                           "\t --output_dir "
+                                           "\t --yolo_dir ")
 
+    parser.add_argument('--input_dir',
+                        action='store',
+                        dest='input_dir',
+                        required=True,
+                        help='Directory where images are stored')
 
+    parser.add_argument('--output_dir',
+                        action='store',
+                        dest='output_dir',
+                        required=True,
+                        help='Directory where output will be stored')
+
+    parser.add_argument('--yolo_dir',
+                        action='store',
+                        dest='yolo_dir',
+                        required=True,
+                        help='Directory where object detector Yolo resides')
+
+    args = parser.parse_args()
+    convert_json(args.input_dir, args.output_dir, args.yolo_dir)
